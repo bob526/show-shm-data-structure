2ae342adc3d471b8ceef8d70b2734f0a0aa0dc78
bob526 <invoid@csie.io>
2016-11-10 14:23:00 +0800

    Solve the shmget problem(use bitwise OR and set the permission)

--- show_shm_ds.c
+++ show_shm_ds.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <time.h>
+#include <fcntl.h>
 /**
     Current Problem:
     The definition of the problem is unclear -> Where does the identifier that will
@@ -9,21 +11,28 @@
     shmget error: Maybe I need to use ftok() to generate a suitable key value.
     shmctl error: Invalid argument????
 **/
+#define STR_MAX_LEN 200
+
 
 int main()
 {
+    srand(time(NULL));
     struct shmid_ds share_mem_buffer;
-    int shm_identifier=0;
-    int shm_key;
+    int shm_identifier=0,shm_key=0;
+    int temp_file_descriptor;
+    char filename_str[STR_MAX_LEN]="keystring_XXXXXX";
 
-    //puts("Please input the identifier for a share-memory segment");
-    //scanf("%d", &shm_identifier);
-    puts("Please input the key you want to use for the share-memory segment.");
-    scanf("%d",&shm_key);
+    temp_file_descriptor = mkstemp(filename_str);
+    if (temp_file_descriptor==-1) {
+        perror("mkstemp error");
+        return 0;
+    }
+    shm_key = ftok(filename_str,(rand()%250+1));
 
+
+    //Create a shared memory with permission that owner can read and write.
     //IPC_CREAT = if shm exist -> return it, else -> create new one
-
-    shm_identifier = shmget(shm_key,40960,IPC_CREAT&IPC_EXCL);
+    shm_identifier = shmget(shm_key,40960,IPC_CREAT|IPC_EXCL|S_IRUSR|S_IWUSR);
     if (shm_identifier==-1) {
         perror("shmget error");
         return 0;
@@ -36,5 +45,6 @@ int main()
 
     printf("Segment ID: %d\nKey: %d\n",shm_identifier,share_mem_buffer.shm_perm.__key);
 
+    remove(filename_str);
     return 0;
 }
