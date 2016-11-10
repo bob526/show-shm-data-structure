2ceb38c532726ea12f165db172e6ed37b3345b94
bob526 <invoid@csie.io>
2016-11-10 20:56:54 +0800

    Readible mode text & delete some comment

--- show_shm_ds.c
+++ show_shm_ds.c
@@ -9,7 +9,6 @@
     The definition of the problem is unclear -> Where does the identifier that will
         be passed come from?
     shmget error: Maybe I need to use ftok() to generate a suitable key value.
-    shmctl error: Invalid argument????
 **/
 #define STR_MAX_LEN 200
 #define SHM_SIZE 4096
@@ -21,6 +20,7 @@ int main()
     int shm_identifier=0,shm_key=0;
     int temp_file_descriptor;
     char filename_str[STR_MAX_LEN]="keystring_XXXXXX";
+    unsigned short save_mode=0;
 
     //Create a temp file and use its pathname to get a shm_key.
     temp_file_descriptor = mkstemp(filename_str);
@@ -33,7 +33,7 @@ int main()
 
     //Create a shared memory with permission that owner can read and write.
     //IPC_CREAT = if shm exist -> return it, else -> create new one
-    shm_identifier = shmget(shm_key,SHM_SIZE,IPC_CREAT|IPC_EXCL|S_IRUSR|S_IWUSR);
+    shm_identifier = shmget(shm_key,SHM_SIZE,IPC_CREAT/*|IPC_EXCL*/|S_IRUSR|S_IWUSR);
     if (shm_identifier==-1) {
         perror("shmget error");
         return 0;
@@ -49,6 +49,23 @@ int main()
     printf("Segment ID: %d\nKey: %d\n",shm_identifier,share_mem_buffer.shm_perm.__key);
     printf("Mode: %hu\nOwner UID: %d\n",share_mem_buffer.shm_perm.mode,share_mem_buffer.shm_perm.uid);
     printf("Size: %lu\nNumber of attaches:%lu\n",share_mem_buffer.shm_segsz,share_mem_buffer.shm_nattch);
+
+    save_mode = share_mem_buffer.shm_perm.mode;
+
+    //Output readible mode text
+    puts("\nThe permission of the share-memory segment is:");
+    if (save_mode & 0400)
+        printf("Read permission of owner.\n");
+    if (save_mode & 0200)
+        printf("Write permission of owner.\n");
+    if (save_mode & 0040)
+        printf("Read permission of group.\n");
+    if (save_mode & 0020)
+        printf("Write permission of group.\n");
+    if (save_mode & 0004)
+        printf("Read permission of world.\n");
+    if (save_mode & 0002)
+        printf("Write permission of world.\n");
 
     //Delete temp file
     remove(filename_str);
